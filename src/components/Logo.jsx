f273ca36c6021d0262408cce788ef7ec4cd4d03d
Kevin <qeven03@gmail.com>
2023-10-24 13:30:15 -0500

    football player quiz game

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,27 +6,26 @@ export default function Logo({
 
 
 const [logo, setLogo] = useState('')
+const [error, setError] = useState(null)
 
 useEffect(() => {
     async function fetching() {
         try {    
-         const response = await fetch(
-            `https://apiv2.allsportsapi.com/football/?&met=Teams&teamName=${club}&APIkey=ecd1bf46e9d359b5c69596e04df7ada0bf65f2b3cf87484ce04721fa4a0b2aa4`, 
-            {mode:'cors'}
-            )
-         const data = await response.json()
-         setLogo(data.result[0].team_logo)
-         }
+            const response = await fetch(
+                `https://apiv2.allsportsapi.com/football/?&met=Teams&teamName=${club}&APIkey=ecd1bf46e9d359b5c69596e04df7ada0bf65f2b3cf87484ce04721fa4a0b2aa4`, 
+                {mode:'cors'}
+                )
+            const data = await response.json()
+            setLogo(data.result[0].team_logo)
+        }
         catch(err) {
-         console.log('noo')
+            setError(err)
         }
      }
     fetching()
 },[])
-     
-    return (
-        <>
-            <div className="logo"><img className="image" src={logo} alt={club}/></div>
-        </>
-    )
+
+    if (error) return <div className="logo">{error}</div>
+    
+    return <div className="logo"><img className="image" src={logo} alt={club}/></div>
 }
